d82321bd17732396d0c6ac1aceaa5d4e595c7bdd
Renzo Damian <renzo.damian.go@gmail.com>
2023-03-11 00:50:54 -0500

    Add nav

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,15 @@
+<nav>
+ <ul>
+   <li><a href="/">Home</a></li>
+   <li><a href="/about/">About</a></li>
+ </ul>
+</nav>
+
+<style lang="sass">
+ nav ul {
+   list-style: none;
+   padding: 3px 5px;
+   background: #111;
+
+ }
+</style>
